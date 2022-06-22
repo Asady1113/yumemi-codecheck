3e8b3b5a54c564885735e798fbf09b08289e1366
Asady1113 <asadyswift@gmail.com>
2022-06-23 00:30:58 +0900

    DetailVCのUI分離

--- iOSEngineerCodeCheck/DetailViewController.swift
+++ iOSEngineerCodeCheck/DetailViewController.swift
@@ -10,35 +10,22 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
-    @IBOutlet weak var ownerImageView: UIImageView!
-    
-    @IBOutlet weak var titleLabel: UILabel!
-    
-    @IBOutlet weak var languageLabel: UILabel!
-    
-    @IBOutlet weak var starsCountLabel: UILabel!
-    @IBOutlet weak var watcherCountLabel: UILabel!
-    @IBOutlet weak var forkCountLabel: UILabel!
-    @IBOutlet weak var issueCountLabel: UILabel!
+    //UIはDetailViewファイルで作成
+    @IBOutlet weak var detailView : DetailView!
     
     var rootVC: RootViewController!
         
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        //文字数に合わせてフォントサイズを調整
-        titleLabel.adjustsFontSizeToFitWidth = true
-        languageLabel.adjustsFontSizeToFitWidth = true
-        
         //RootViewControllerで選択されたrepositoryを代入
         let repo = rootVC.repositoryArray[rootVC.selectedIndex]
         
-        languageLabel.text = "Written in \(repo["language"] as? String ?? "")"
-        starsCountLabel.text = "\(repo["stargazers_count"] as? Int ?? 0) stars"
-        watcherCountLabel.text = "\(repo["wachers_count"] as? Int ?? 0) watchers"
-        forkCountLabel.text = "\(repo["forks_count"] as? Int ?? 0) forks"
-        issueCountLabel.text = "\(repo["open_issues_count"] as? Int ?? 0) open issues"
+        detailView.languageLabel.text = "Written in \(repo["language"] as? String ?? "")"
+        detailView.starsCountLabel.text = "\(repo["stargazers_count"] as? Int ?? 0) stars"
+        detailView.watcherCountLabel.text = "\(repo["wachers_count"] as? Int ?? 0) watchers"
+        detailView.forkCountLabel.text = "\(repo["forks_count"] as? Int ?? 0) forks"
+        detailView.issueCountLabel.text = "\(repo["open_issues_count"] as? Int ?? 0) open issues"
         
         
         getOwner()
@@ -50,7 +37,7 @@ class DetailViewController: UIViewController {
         
         let repo = rootVC.repositoryArray[rootVC.selectedIndex]
         
-        titleLabel.text = repo["full_name"] as? String
+        detailView.titleLabel.text = repo["full_name"] as? String
         
         if let owner = repo["owner"] as? [String: Any] {
             
@@ -71,7 +58,7 @@ class DetailViewController: UIViewController {
                 let img = UIImage(data: data!)!
                 
                 DispatchQueue.main.async {
-                    self.ownerImageView.image = img
+                    self.detailView.ownerImageView.image = img
                 }
                 
             }.resume()
